ef86508f34c289c8ef8d90321f8330144db78a49
tae2089 <tae2089002@gmail.com>
2021-08-10 23:58:51 +0900

    kafka admin api code 추가

--- kafka/kafka-java/app/src/main/java/elastic/Admin.java
+++ kafka/kafka-java/app/src/main/java/elastic/Admin.java
@@ -2,6 +2,7 @@ package elastic;
 
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.DescribeConfigsResult;
+import org.apache.kafka.clients.admin.TopicDescription;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.Node;
 import org.apache.kafka.common.config.ConfigResource;
@@ -9,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
@@ -18,17 +20,22 @@ public class Admin {
     private static final String BOOTSTRAP_SERVERS_CONFIG = "";
     private static final Logger logger = LoggerFactory.getLogger(Admin.class);
 
-    public AdminClient getAdmin() throws ExecutionException, InterruptedException {
-        Properties configs = new Properties();
-        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
-
-        return AdminClient.create(configs);
+    public AdminClient getAdmin()  {
+        try{
+            Properties configs = new Properties();
+            configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
+            return AdminClient.create(configs);
+        }catch (Exception e) {
+            return null;
+        }
     }
 
     public void getBrokerInfo() {
+        AdminClient admin = getAdmin();
         try{
             logger.info("start getBrokerInfo func");
-            AdminClient admin = getAdmin();
+
+
             for (Node node: admin.describeCluster().nodes().get()){
                 logger.info("node: {}", node);
                 ConfigResource cr = new ConfigResource(ConfigResource.Type.BROKER, node.idString());
@@ -40,8 +47,18 @@ public class Admin {
         }catch (Exception e) {
             logger.info(e.getMessage());
         }
-
-
+        admin.close();
     }
 
+    public void getTopicInfo() {
+        AdminClient admin = getAdmin();
+        try{
+            logger.info("start getBrokerInfo func");
+            Map<String, TopicDescription> topicInfo = admin.describeTopics(Collections.singletonList("test")).all().get();
+            logger.info("topicInfo: {}", topicInfo);
+        }catch (Exception e) {
+            logger.info(e.getMessage());
+        }
+        admin.close();
+    }
 }
